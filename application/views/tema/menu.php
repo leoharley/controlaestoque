c87851fc61ea8f4b09712d9c6fda21ea6864256b
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-02-22 22:06:28 -0300

    ....

--- application/views/tema/menu.php
+++ application/views/tema/menu.php
@@ -1,11 +1,11 @@
 <!--sidebar-menu-->
 <nav id="sidebar">
     <div id="newlog">
-        <div class="icon2">
+       <!-- <div class="icon2">
             <img src="<?php echo base_url() ?>assets/img/logo-two.png">
-        </div>
+        </div> -->
         <div class="title1">
-            <?= $configuration['app_theme'] == 'white' ||  $configuration['app_theme'] == 'whitegreen' ? '<img src="' . base_url() . 'assets/img/logo-mapos.png">' : '<img src="' . base_url() . 'assets/img/logo-mapos-branco.png">'; ?>
+            <?= $configuration['app_theme'] == 'white' ||  $configuration['app_theme'] == 'whitegreen' ? '<img src="' . base_url() . 'assets/img/logo-controlaestoque.png">' : '<img src="' . base_url() . 'assets/img/logo-mapos-branco.png">'; ?>
         </div>
     </div>
     <a href="#" class="visible-phone">
