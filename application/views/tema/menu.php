f2b49d4d5ab6bf9167b326a16e87df6ad534d45d
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-02-22 22:37:10 -0300

    ....

--- application/views/tema/menu.php
+++ application/views/tema/menu.php
@@ -2,10 +2,10 @@
 <nav id="sidebar">
     <div id="newlog">
         <div class="icon2">
-            <img src="<?php echo base_url() ?>assets/img/icone-controlaestoque.png">
+            <img src="<?php echo base_url() ?>assets/img/icone-controlaestoque.png" style="max-width: 140%">
         </div>
         <div class="title1">
-            <?= $configuration['app_theme'] == 'white' ||  $configuration['app_theme'] == 'whitegreen' ? '<img src="' . base_url() . 'assets/img/logo-controlaestoque.png">' : '<img src="' . base_url() . 'assets/img/logo-mapos-branco.png">'; ?>
+            <?= $configuration['app_theme'] == 'white' ||  $configuration['app_theme'] == 'whitegreen' ? '<img src="' . base_url() . 'assets/img/logo-sotexto-controlaestoque.png">' : '<img src="' . base_url() . 'assets/img/logo-mapos-branco.png">'; ?>
         </div>
     </div>
     <a href="#" class="visible-phone">
