9c7c1aa3f64245863a20f161cda89e2284ab6dbe
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-03-08 22:59:43 -0300

    ...

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -87,7 +87,7 @@ class Produtos extends MY_Controller
 
     public function editar()
     {
-        if ($this->uri->segment(3) == null || !is_numeric($this->uri->segment(3))) {
+        if ($this->uri->segment(3) == null) {
             $this->session->set_flashdata('error', 'Item não pode ser encontrado, parâmetro não foi passado corretamente.');
             redirect('mapos');
         }
